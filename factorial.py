3ac8092b874848c58e5e57fb65de3b2303961b78
aka-happy <harsh.dgis@gmail.com>
2025-08-22 18:43:37 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -9,3 +9,26 @@ while(number>0):
 
 print(fact)
 
+
+# using for loop
+
+n = int(input("Enter your number: "))
+
+fact = 1
+for i in range(1, n+1):
+    fact*= i
+
+print(fact)
+
+
+# using recursion function
+
+def fact(n):
+    if(n == 1):
+        return 1    
+    else:
+        return n*fact(n-1)
+    
+print(fact(5))
+
+
